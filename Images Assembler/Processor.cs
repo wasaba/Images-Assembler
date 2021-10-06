41e9f2953a32132144c8bc3f8878a41bd488aa94
wasaba <patafix258@gmail.com>
2021-10-06 21:00:47 +0200

    processor structure

--- Images Assembler/Processor.cs
+++ Images Assembler/Processor.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Image_Assembler
 {
 	public class Processor
 	{
-		public Processor()
+
+		public void Assemble(byte[] Pic1, byte[] Pic2)
 		{
 
 		}
+
+		public void Save(Image image, string path)
+        {
+
+        }
 	}
 }
